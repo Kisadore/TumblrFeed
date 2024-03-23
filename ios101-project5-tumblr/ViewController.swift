e6b36f5527ed9d4b730523bf63264b889c2744cb
Kiahna Isadore <kiahnaisadore@gmail.com>
2024-03-23 18:10:56 -0500

    completed project

--- ios101-project5-tumblr/ViewController.swift
+++ ios101-project5-tumblr/ViewController.swift
@@ -6,19 +6,48 @@
 import UIKit
 import Nuke
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TumblrCell", for: indexPath) as! TumblrCell
+        let post = posts[indexPath.row]
+        //cell.textLabel?.text = "Row \(indexPath.row)"
+        
+        if let photo = post.photos.first {
+            let url = photo.originalSize.url
+            // Load the photo in the image view via Nuke library...
+            Nuke.loadImage(with: url, into: cell.posterImageView)
+        }
+        cell.feedLabel.text = post.summary
+        
+        return cell
+    }
+    
 
-
+    @IBOutlet weak var tableView: UITableView!
+  
+    private var posts: [Post] = []
+    private var refreshControl = UIRefreshControl()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
         
+        // Add refresh control to the table view
+        tableView.addSubview(refreshControl)
+        
+        // Add target-action pair to call fetchPosts method when refresh control is triggered
+        refreshControl.addTarget(self, action: #selector(fetchPosts), for: .valueChanged)
+                
         fetchPosts()
     }
 
 
-
-    func fetchPosts() {
+    @objc func fetchPosts() {
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork/posts/photo?api_key=1zT8CiXGXFcQDyMFG7RtcfGLwTdDjFUJnZzKJaWTmgyK4lKGYk")!
         let session = URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
@@ -48,7 +77,14 @@ class ViewController: UIViewController {
                     for post in posts {
                         print("🍏 Summary: \(post.summary)")
                     }
+                    
+                    self?.posts = posts
+                    self?.tableView.reloadData()
+                    
+                    // End refreshing process
+                    self?.refreshControl.endRefreshing()
                 }
+                
 
             } catch {
                 print("❌ Error decoding JSON: \(error.localizedDescription)")
